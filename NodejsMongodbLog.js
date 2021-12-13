2747a015b53adca2a7729aadb0bd287b1409c1fb
wt <1376726084@qq.com>
2021-12-12 17:36:37 -0800

    ejs is basicall successful

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const mongoose = require('mongoose');
+const ejs = require('ejs');
 mongoose.connect('mongodb://172.21.2.236/190110910820');
 const app = express()
 const schema={
@@ -11,10 +12,17 @@ const schema={
 const Data = mongoose.model('data1',schema);
 app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
-    res.send(req.query)
+    //res.send(req.query)
     console.log(req.query)
     const kitty = new Data({name:req.query.first,data2:req.query.submit1});
     kitty.save().then(()=>console.log('write ok'))
+    
+    //ejs.renderFile(filename,data,options,function(err,str){
+        //str=>输出选然后的html字符串
+    //});
+    ejs.renderFile('result.html',{returnVal:'success'},(err,str)=>{
+        res.send(str)
+    });
 })
 
 app.listen(10820)
